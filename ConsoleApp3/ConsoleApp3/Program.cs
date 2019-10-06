79e4bb6f720b35b901027ba8b86e01baf93ef8d4
Igor <rico210448@gmail.com>
2019-10-06 23:26:46 +0300

    laba 1-2 , IT academy

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int decade = 10;
+
+
+       
+
+            Console.WriteLine("Please enter a number");
+
+
+            long DateEntryAndConvertToLong()
+            {
+                long inputNumber;
+                String inputDate = Console.ReadLine();
+
+                bool result = false;
+                do
+                {
+                    result = Int64.TryParse(inputDate, out inputNumber);
+                    if (!result)
+                    {
+                        Console.WriteLine("You have entered incorrect date. Try again");
+                        inputDate = Console.ReadLine();
+                      
+                    }
+                    if (inputNumber < 0)
+                    {
+                        Console.WriteLine("You have entered incorrect date. Try again");
+                        inputDate = Console.ReadLine();
+                        result = false;
+                    }
+                } while (!result);
+
+                return inputNumber;
+            }
+
+            long inputDigit = DateEntryAndConvertToLong();
+
+
+
+
+            //пункт 1
+            int HowMuchNumbers()
+            {
+             
+                int numberOfDigits = 0;
+                if (inputDigit == 1 || inputDigit == 0)
+                {
+                    numberOfDigits = 1;
+                }
+                else
+                {
+                    for (long i = 1; i < inputDigit; i = i * decade)
+                    {
+                        numberOfDigits = numberOfDigits + 1;
+                    }
+                }
+                return numberOfDigits;
+            }
+           
+
+            //підрахувати степінь
+            long CalculateDegree()
+            {
+                long degree = decade;
+                for (int i = 1; i < HowMuchNumbers()-1; i++)
+                {
+                    degree = degree * decade;
+                }
+                return degree;
+            }
+            // повертає число  потрібного індексу
+            int ReturnINumber(long numeric, int neededNumber)
+            {
+                
+                int digitWithNeededNumber=0;
+                long degree = CalculateDegree();
+                for (int i = 0; i < HowMuchNumbers(); ++i)
+                {
+                    digitWithNeededNumber = (int)(numeric / degree);
+                    if(i== neededNumber - 1)
+                    {
+                        break;
+                    }
+                    numeric = numeric - (digitWithNeededNumber * degree);
+                    degree = degree / decade;
+                }
+                return digitWithNeededNumber;
+
+            }
+
+            //пункт 2
+            void АrrayOfNumbers(long numeric)
+            {
+                int[] elementOfInput = new int[HowMuchNumbers()];
+                int needeNumber = 1;
+
+               for(int i=0;i< elementOfInput.Length; i++)
+                {
+                    elementOfInput[i] = ReturnINumber(numeric, needeNumber);
+                    needeNumber = needeNumber + 1;
+                  
+                }
+
+                for (int i = 0; i < elementOfInput.Length; i++)
+                {
+                    Console.Write($" { elementOfInput[i]}");
+                }
+
+            }
+
+            // пункт 3
+            float ArithmeticMean(long numeric)
+            {
+                float arithmeticMean=0;
+                int sum= 0;
+                for(int i = 1; i <= HowMuchNumbers(); i++)
+                {
+                    sum = sum + ReturnINumber(numeric, i);
+
+                }
+                arithmeticMean = (float)sum / HowMuchNumbers();
+
+
+                return arithmeticMean;
+
+            }
+           
+            // пункт 4
+            double GeometricMean(long numeric)
+            {
+                double geometricMean = 0;
+                double product = 1;
+                for (int i = 1; i <= HowMuchNumbers(); i++)
+                {
+                    product = product * ReturnINumber(numeric, i);
+
+                }
+
+                geometricMean = Math.Pow(product, (double)1 / HowMuchNumbers());
+
+                return geometricMean;
+               
+
+            }
+
+            // пункт 5
+            long Factorial(long numeric)
+            {
+                long factorial = 1;
+                
+                while (numeric > 0)
+                {
+
+                    factorial = factorial * numeric ;
+                    
+                    numeric = numeric - 1;
+                }
+
+                return factorial;
+            }
+
+            // пункт 6 
+            long SumEvenNumbers(long numeric)
+            {
+                long sum = 0;
+                for (int i = 1; i <= numeric; i++)
+                {
+                    if(i % 2 == 0) 
+                    {
+                        sum = sum + i;
+
+                    }
+
+                }
+                return sum;
+            }
+
+            // пункт 7
+            long SumOddNumbers(long numeric)
+            {
+                long sum = 0;
+                for (int i = 1; i <= numeric; i++)
+                {
+                    if (i % 2 == 1)
+                    {
+                        sum = sum + i;
+
+                    }
+
+                }
+                return sum;
+            }
+
+            //пункт 8
+            long OverloadedSumEvenNumbers(long startOfReference , long numeric)
+            {
+                long sum = 0;
+                while (startOfReference<= numeric) {
+                    if (startOfReference % 2 == 0)
+                    {
+                        sum = sum + startOfReference;
+
+                    }
+                    startOfReference++;
+                }
+                
+                return sum;
+            }
+            //пункт 9
+            long OverloadedSumOddNumbers(long startOfReference, long numeric)
+            {
+                long sum = 0;
+                while (startOfReference <= numeric)
+                {
+                    if (startOfReference % 2 == 1)
+                    {
+                        sum = sum + startOfReference;
+
+                    }
+                    startOfReference++;
+                }
+
+                return sum;
+            }
+
+            Console.WriteLine($"Number of digits in the entered number  = {HowMuchNumbers()}");
+            Console.Write($"Array of digits of the entered number - ");
+            АrrayOfNumbers(inputDigit);
+            Console.WriteLine();
+            Console.WriteLine($"The arithmetic mean of the entered number = {ArithmeticMean(inputDigit)}");
+            Console.WriteLine($"The geometric mean of the entered number = {GeometricMean(inputDigit)}");
+            Console.WriteLine($"Factorial of the entered number = {Factorial(inputDigit)}");
+            Console.WriteLine($"The sum of all even numbers from 1 to the entered number = {SumEvenNumbers(inputDigit)}");
+            Console.WriteLine($"The sum of all odd numbers from 1 to the entered number = {SumOddNumbers(inputDigit)}");
+            Console.WriteLine("Please enter a range of numbers (by two numbers)");
+            Console.WriteLine($"enter first number");
+            long n = DateEntryAndConvertToLong();
+            Console.WriteLine($"enter second number");
+            long m = DateEntryAndConvertToLong();
+
+            Console.WriteLine($"The sum of all even numbers in range of numbers = {OverloadedSumEvenNumbers(n,m)}");
+            Console.WriteLine($"The sum of all odd numbers in range of numbers = {OverloadedSumOddNumbers(n, m)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ laba2/laba2/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str1;
+            String str2;
+            Console.WriteLine("Enter string № 1");
+            str1 = Console.ReadLine();
+            Console.WriteLine("Enter string № 2");
+            str2 = Console.ReadLine();
+
+            
+
+            List<char> uniqeCharStr1 = new List<char>();
+           List<char> uniqeCharStr2 = new List<char>();
+
+            void CreateUniqeListFromString(String inputString, List<char> uniqeList)
+            {
+                for (int i = 0; i < inputString.Length; i++)
+                {
+                    char[] s = inputString.ToCharArray();
+                    if (uniqeList.Count == 0)
+                    {
+                        uniqeList.Add(s[i]);
+                    }
+                    else
+                    {
+                        for (int j = 0; j < uniqeList.Count; j++)
+                        {
+
+                            if (s[i] == uniqeList[j])
+                            {
+                                break;
+                            }
+                            if (j == uniqeList.Count - 1)
+                            {
+                                uniqeList.Add(s[i]);
+                            }
+
+                        }
+                    }
+                }
+                
+            }
+            void ClearVector(List<char> listWhichCleaning)
+            {
+                int listSize = listWhichCleaning.Count;
+                for (int i =0; i < listSize; i++)
+                {
+                    listWhichCleaning.RemoveAt(0);
+                    
+                }
+            }
+
+            bool IfString1ContainString2(string string1,string string2)
+            {
+                bool ifHave = true; ;
+                CreateUniqeListFromString(string1, uniqeCharStr1);
+                CreateUniqeListFromString(string2,
+                    uniqeCharStr2);
+                int notSame= 0;
+                if (uniqeCharStr2.Count == 0)
+                {
+                    ifHave = true;
+                }
+                else
+                {
+                    for (int i = 0; i < uniqeCharStr2.Count; i++)
+                    {
+                        for (int j = 0; j < uniqeCharStr1.Count; j++)
+                        {
+                            if (uniqeCharStr2[i] == uniqeCharStr1[j])
+                            {
+                                notSame = 0;
+                                break;
+                            }
+                            else
+                            {
+                                notSame = notSame + 1;
+                            }
+                            
+                        }
+                        if(notSame == uniqeCharStr1.Count)
+                        {
+                            ifHave = false;
+                            break;
+                        }
+                    }
+                }
+                ClearVector(uniqeCharStr1);
+                ClearVector(uniqeCharStr2);
+                return ifHave;
+            }
+
+            void СhangeSameSymbolsString1(string string1, string string2)
+            {
+
+                char[] s1 = string1.ToCharArray();
+                char[] s2 = string2.ToCharArray();
+                for (int i = 0; i < string2.Length; i++)
+                {
+                    for (int j = 0; j < string1.Length; j++)
+                    {
+                        if (s2[i] == s1[j])
+                        {
+                            s1[j] = '#';
+
+
+                        }
+                    }
+                }
+            
+                Console.WriteLine("New String 1 are looking as  ");
+                for (int i = 0; i < str1.Length; i++)
+                {
+                    Console.Write(s1[i]);
+                }
+            }
+
+         
+            int CountRepetition(string string1, string string2)
+            {
+                string temp = string1.Replace(str2, "");
+                int result = (str1.Length - temp.Length) / str2.Length;
+                return result;
+
+
+            }
+
+            Console.WriteLine();
+            if (IfString1ContainString2(str1, str2))
+            {
+                Console.WriteLine("String №1 contains symbol of String № 2");
+            }
+            else
+            {
+                Console.WriteLine("String №1 not contain String № 2");
+            }
+            Console.WriteLine();
+            СhangeSameSymbolsString1(str1, str2);
+            Console.WriteLine();
+            if (CountRepetition(str1, str2) == 0)
+            {
+                Console.WriteLine("String №2 not occur in String № 1");
+            }
+            else
+            {
+                Console.WriteLine($"String №2  occurs in String № 1 - {CountRepetition(str1, str2)}  times");
+            }
+        
+
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
